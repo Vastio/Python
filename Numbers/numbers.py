75a942dac333700c80e2e6e5b6bc9d129c563669
Seba <freevoyager@autistici.org>
2016-01-24 18:14:53 +0100

    Aggiunte due nuove funzioni.

--- Numbers/numbers.py
+++ Numbers/numbers.py
@@ -11,11 +11,22 @@
 
 
 import sys, os, time
-import json, urllib2, re
+import argparse, json, urllib2, re
 import smtplib, imaplib, email.utils
 from email.mime.text import MIMEText
 from bs4 import BeautifulSoup
 
+
+#----------------
+# Define Version
+#----------------
+__version__ = 'v0.3'
+
+
+
+#------------
+# Globa Vars
+#------------
 configFile = "./config.json"
 
 
@@ -142,9 +153,52 @@ def extractNewNumbersFromMail(mailFrom, debug) :
 
 
 #
+## Aggiunge una nuova mail al file di configurazione
+def addNewMail(new_mail) :
+
+    mail_list = {'mailTo' : []}
+
+    try :
+        json_data = None
+        # Apre il file config.json in lettura
+        with open(config_file, 'r') as j_file :
+            json_data = json.load(j_file)
+
+            # Aggiunge la nuova mail se non esiste gia
+            if not new_mail in json_data['mailTo'] :
+                json_data['mailTo'].append(new_mail)
+
+        # Apre il file in scrittura e scrive il dati cambiati
+        with open(config_file, 'w') as j_file :
+            j_file.write(json.dumps(json_data))
+
+    except IOError, err :
+        print "[!] Error to open config file: %s" % err
+        exit(1)
+    except ValueError, err :
+		print "[!] Error in json file: %s" % err
+		exit(1)
+
+##
+
+
+
+#
 ## MAIN
 def main() :
+
+     # Command line arguments
+    parser = argparse.ArgumentParser(description="Compare numbers")
+    parser.add_argument('-m', '--mail', help="add new mailTo to config file and exit.")
+    parser.add_argument('--version', action='version', version=__version__)
+    args = parser.parse_args()
+
     json = loadJson() # Carica i dati da json
+
+     # Aggiunge unsa nuova mail al file di configurazione
+    if args.mail :
+        addNewMail(args.mail)
+        sys.exit(1)
 
     extractNums = getExtractNumbers(json['url'])
     if extractNums == None :
