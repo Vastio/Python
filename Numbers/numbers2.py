687e0b5aa0e645f7d63503a4b851346dbdf2f7ec
Seba <freevoyager@autistici.org>
2017-11-25 16:52:27 +0100

    Function loadJson

--- Numbers/numbers2.py
+++ Numbers/numbers2.py
@@ -1,3 +1,16 @@
+import json
+
+
+configFile = "./config.json"
+
+
+# Load json config file
+def loadJson():
+    with open(configFile) as json_file:
+        jsonData = json.load(json_file)
+    return jsonData
+###
+
 
 def main():
     pass
